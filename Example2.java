ec80c6e2c9d76776ca4dbfda439cac8a7f8759b6
AgalyaSri1210 <66957945+AgalyaSri1210@users.noreply.github.com>
2020-06-15 20:18:01 +0530

    Add files via upload
    upload1

---
+++ Example2.java
@@ -0,0 +1,9 @@
+class Example2{
+	public static void main(String args[]){
+		if(args.length !=0){
+			System.out.println("Welcome "+args[0]);
+		}else{
+			System.out.println("Need argument");
+		}
+	}
+}
---
+++ Example3.java
@@ -0,0 +1,12 @@
+class Example3{
+	public static void main(String args[]){
+		if(args.length==2){
+			int num1=Integer.parseInt(args[0]);
+			int num2=Integer.parseInt(args[1]);
+			System.out.println("The sum of "+num1+" and "+num2+" is "+(num1+num2));
+		}
+		else{
+			System.out.println("Enter two numbers");
+		}
+	}
+}
